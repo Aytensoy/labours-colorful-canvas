4db66c2ef81fdb8d219e1066471014969fea949f
Aytensoy <157885112+Aytensoy@users.noreply.github.com>
2026-08-11 07:32:41 +0300

    Update assetlinks.json for TWA, make contact email clickable, and bump service worker version

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // =================================================================
-// SERVICE WORKER - NİHAİ, GÜVENLİ VE TAM KAPSAMLI SÜRÜM (A53)
+// SERVICE WORKER - NİHAİ, GÜVENLİ VE TAM KAPSAMLI SÜRÜM (A55)
 // =================================================================
 
-const CACHE_NAME = 'magical-coloring-A53';
+const CACHE_NAME = 'magical-coloring-A55';
 
 const FILES_TO_CACHE = [
     // Çekirdek Dosyalar
