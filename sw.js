e08b9a653b5dac94e57c0e0bd823cfafb64d0f0d
Aytensoy <157885112+Aytensoy@users.noreply.github.com>
2026-08-13 20:44:23 +0300

    Add Google Play App Signing fingerprint to assetlinks

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // =================================================================
-// SERVICE WORKER - NİHAİ, GÜVENLİ VE TAM KAPSAMLI SÜRÜM (A55)
+// SERVICE WORKER - NİHAİ, GÜVENLİ VE TAM KAPSAMLI SÜRÜM (A56)
 // =================================================================
 
-const CACHE_NAME = 'magical-coloring-A55';
+const CACHE_NAME = 'magical-coloring-A56';
 
 const FILES_TO_CACHE = [
     // Çekirdek Dosyalar
